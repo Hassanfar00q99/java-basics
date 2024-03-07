ab5b5583eef1eee5039231243338bc05089ca697
Hassanfar00q99 <162594532+Hassanfar00q99@users.noreply.github.com>
2024-03-07 17:54:04 +0000

    javascript

---
+++ 01_basics/datatype_conversion.js
@@ -0,0 +1,21 @@
+let score= 33
+let grade= "78kdj"
+console.log(typeof score);
+console.log(typeof grade);
+let ScoreInMath= Number(grade)
+// console.log(typeof ScoreInMath);
+// console.log(ScoreInMath) 
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let StringNumber= 99
+let convertNumber= String(StringNumber)
+// console.log(convertNumber);
+// console.log(typeof (convertNumber));
+
+let a= 4
+let b =a++;
+console.log(b);
+console.log(a);
+// prefix wo hota hai jis mien phly value add hoti hai or phir 2sry mien or postfix isky ult hota hai
