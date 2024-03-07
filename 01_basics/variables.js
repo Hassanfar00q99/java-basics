16eb24723e47f49c644e3ec67567967ded06ab6b
Hassanfar00q99 <162594532+Hassanfar00q99@users.noreply.github.com>
2024-03-07 15:49:02 +0000

    javascript

---
+++ 01_basics/variables.js
@@ -0,0 +1,7 @@
+const accoundId = 1122
+let accountEmail = "hassanfrooq@gmail.com"
+var accountPassword = "896627"
+City = "pakistan"
+accountemail="kojhalarka@gmail.com"
+let accountState;
+console.table([accoundId,accountEmail,accountPassword,City]);
