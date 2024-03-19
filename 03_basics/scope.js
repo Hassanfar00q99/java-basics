12e7c844aa76dbc5b1c2800e85e4bb6642b27684
Hassanfar00q99 <162594532+Hassanfar00q99@users.noreply.github.com>
2024-03-19 10:58:12 +0000

    scope

---
+++ 03_basics/scope.js
@@ -0,0 +1,26 @@
+let a =89
+    if (true){
+        let a = 8
+        // return a
+    }
+
+// console.log(user(a));
+console.log(a);
+let b="ks"
+console.log(b);
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+console.log(one().two());
