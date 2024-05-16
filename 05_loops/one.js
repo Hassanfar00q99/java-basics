a4f78ecccc20805c72d0a7acd747a6677369ff1f
Hassanfar00q99 <162594532+Hassanfar00q99@users.noreply.github.com>
2024-05-16 20:58:36 +0000

    loops

---
+++ 05_loops/one.js
@@ -0,0 +1,17 @@
+for (let h = 5; h < 10; h++) {
+    const element = h;
+    console.log(`outer loop ${h}`);
+    for (let a = 0; a < 7; a++) {
+        const element = a;
+    // console.log(`inner loop ${a} outer loop ${h}`);
+    console.log(`${h} * ${a} = ${h*a}`);
+        
+    }
+} 
+
+const greetings = "Hello 5 world! ";
+for (const greet of greetings) {
+    if (greet !== " ") {
+        console.log(`Each char is ${greet}`);
+    }
+}
